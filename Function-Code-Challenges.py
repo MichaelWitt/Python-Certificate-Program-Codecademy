f5c10b183633d2c7f317f4b895cb15151237b55a
Michael W <mikewittorp@gmail.com>
2021-02-28 09:33:23 -0500

    Update Function-Code-Challenges.py

--- Function-Code-Challenges.py
+++ Function-Code-Challenges.py
@@ -43,3 +43,54 @@ def remainder(num1, num2):
 
 print(remainder(15, 14))
 print(remainder(9, 6))
+
+# First 3 Multiples
+
+def first_three_multiples(num):
+  print(num)
+  print(num*2)
+  print(num*3)
+  return num*3
+
+first_three_multiples(10)
+first_three_multiples(0)
+
+# Tip Calculator
+
+def tip(total, percentage):
+  return (total*percentage)/100
+
+print(tip(10, 25))
+print(tip(0, 100))
+
+# James Bond
+
+def introduction(first_name, last_name):
+  bond = last_name + ', ' + first_name + ' ' + last_name
+  return bond
+
+print(introduction("James", "Bond"))
+print(introduction("Maya", "Angelou"))
+
+# Dog Years
+
+def dog_years(name, age):
+  dog_years = age*7
+  return name + ', ' + 'you are ' + str(dog_years) + ' years old in dog years'
+
+print(dog_years("Lola", 16))
+print(dog_years("Baby", 0))
+
+# Math Challenge
+
+def lots_of_math(a, b, c, d):
+  a_plus_b = a+b
+  print(a_plus_b)
+  d_minus_c = d-c
+  print(d_minus_c)
+  a_b_times_d_c = a_plus_b*d_minus_c
+  print(a_b_times_d_c)
+  return a_b_times_d_c % a
+
+print(lots_of_math(1, 2, 3, 4))
+print(lots_of_math(1, 1, 1, 1))
